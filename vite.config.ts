4a0d6a64fdf700ab54369fbfe150a8e5aee940c8
Alexandre Rocha <alxnrocha@gmail.com>
2026-08-16 15:03:38 +0200

    ci: configure peaceiris gh-pages branch deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/gestor-tarefas/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
